a655801a65c1388e0cac62896855d98b32237815
loem-ms <loem.m.aa@m.titech.ac.jp>
2021-02-10 23:38:01 +0900

    Revert "added files and changes for deployment"
    This reverts commit f962970631b47fea2600435a452703014a6911c7.

--- mydayproject/settings.py
+++ mydayproject/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,5 +123,3 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_URL = '/login/'
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
